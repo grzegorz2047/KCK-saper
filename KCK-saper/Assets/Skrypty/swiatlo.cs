159a81407303face32d8d9e884bfb660b2d56529
grzegorz2047 <grzegorz2047@gmail.com>
2016-10-25 16:34:15 +0200

    Zmodyfikowałem trochę mapę.

---
+++ KCK-saper/Assets/Skrypty/swiatlo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+    public Light lt;
+    void Start() {
+        lt = GetComponent<Light>();
+    }
+    void Update() {
+        lt.color -= Color.white / 2.0F * Time.deltaTime;
+    }
+}
