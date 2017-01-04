898973f5fac240fa62f114329f806da4733963e3
apoweska <arkpow1@st.amu.edu.pl>
2017-01-04 11:16:15 +0100

    Update __main__.py
    dodanie remove word
    for dla obiektów
    dziala dla funkcji i obiektów, dodac dla parametrów

--- Python-project/src/__main__.py
+++ Python-project/src/__main__.py
@@ -14,53 +14,6 @@ class Function:
     def __init__(self, function_name):
         self.function_name = function_name
 
-
-def find_function(action):
-    # MIKOLAJ - Pokazuje jak korzystac ze Slownika: szukanie słowa w funkcjach
-    # Analogicznie dla obiektów, parametrów, trzeba tylko zmienić plik
-    # Zakładam, że zdanie zostało już oddzielone po kropce, i masz dostęp do pojedynczych wyrazów.
-    functions_file = ET.ElementTree(file='../Slownik/Funkcje.xml')  #TUTAJ ZMIEŃ PLIK DLA OBIEKTÓW, PARAMETRÓW ITD..
-    root = functions_file.getroot()  # poczatek drzewa
-
-    for function_string in root.findall('funkcja'):  # iteruje po wszystkich funkcjach
-        for spelling in function_string.findall('spelling'):  # dla kazdej mozliwej odmiany
-            if spelling.text.lower() == action.lower(): # lower by ignorowały duże/małe litery
-                return Function(function_string.find('nazwa').text)  # wypisuje nazwe funkcji do wywolania
-    return Function("")
-
-
-def main():
-    queue = Queue()
-    command = get_command()
-    words = get_sentence_from_input_to_list(command)
-    for word in words:
-        function = find_function(word)
-        if find_function(word).function_name != "":
-            print function.function_name
-
-            # save_to_file(get_sentence_from_input_to_list())
-            # visualize()
-
-
-main()
-
-########################################################### AREK - wrzucam moje zadanie
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from Queue import Queue
-
-from extract_actions import *
-from visualize_example import get_command
-import xml.etree.cElementTree as ET
-import pygame
-import map
-import saper
-import bomb
-
-class Function:
-    def __init__(self, function_name):
-        self.function_name = function_name
-
 class Object:
     def __init__(self, object_name):
         self.object_name = object_name
@@ -82,10 +35,10 @@ def find_function(action):
     return Function("")
 
 def find_object(action):
-    objects_file = ET.ElementTree(file='../Slownik/Objekty.xml')  #ścieżka do pliku
+    objects_file = ET.ElementTree(file='../Slownik/Obiekty.xml')  #ścieżka do pliku
     root = objects_file.getroot()
 
-    for object_string in root.findall('objekt'):  # iteruje po wszystkich objektach
+    for object_string in root.findall('obiekt'):  # iteruje po wszystkich objektach
         for spelling in object_string.findall('spelling'):  # dla kazdej mozliwej odmiany
             if spelling.text.lower() == action.lower(): # lower by ignorowały duże/małe litery
                 return Object(object_string.find('nazwa').text)  # wypisuje nazwe objektu do wywolania
@@ -109,26 +62,31 @@ def main():
         function = find_function(word)
         if find_function(word).function_name != "":
             print function.function_name
+            words.remove(word)
+        for word in words:
+            object = find_object(word)
+            if find_object(word).object_name != "":
+                print object.object_name
 
             # save_to_file(get_sentence_from_input_to_list())
             # visualize()
 
-    if function.function_name == "Podnies":
-        object = find_object(word)
-        parameter = find_parameter(word)
-        if find_object(word).object_name != "" and find_parameter(word).parameter_name != "":
-        print (function.function_name , "(" , object.object_name , "," , paramater.parameter_name , ")")
-
-    elif function.function_name == "Zdetonuj":
-        object = find_object(word)
-        parameter = find_parameter(word)
-        if find_object(word).object_name != "" and find_parameter(word).parameter_name != "":
-        print (function.function_name, "(", object.object_name, ")")
-
-    elif function.function_name == "Pojedz":
-        parameter = find_parameter(word)
-        if find_parameter(word).parameter_name != "":
-            print (function.function_name, "(", parameter.parameter_name, ")")
+    # if function.function_name == "Podnies":
+    #     object = find_object(word)
+    #     parameter = find_parameter(word)
+    #     if find_object(word).object_name != "" and find_parameter(word).parameter_name != "":
+    #         print (function.function_name , "(" , object.object_name , "," , parameter.parameter_name , ")")
+    #
+    # elif function.function_name == "Zdetonuj":
+    #     object = find_object(word)
+    #     parameter = find_parameter(word)
+    #     if find_object(word).object_name != "" and find_parameter(word).parameter_name != "":
+    #         print (function.function_name, "(", object.object_name, ")")
+    #
+    # elif function.function_name == "Pojedz":
+    #     parameter = find_parameter(word)
+    #     if find_parameter(word).parameter_name != "":
+    #         print (function.function_name, "(", parameter.parameter_name, ")")
 
 main()
 
